e24aeed3bc3f749e70235ffe606da8068c72edfa
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-09-28 12:15:54 -0300

    Criando navigate esquerda e direita na lista de filmes

--- src/components/MovieRow/index.js
+++ src/components/MovieRow/index.js
@@ -1,11 +1,40 @@
 import './MovieRow.css'
+import {useState} from 'react'
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 export default function MovieRow({title, itens}) {
+    const [scrollX, setScrollX] = useState(0);
+
+    const handleLeftArrow = () => {
+        let x = scrollX + Math.round(window.innerWidth / 2);
+        if (x > 0) {
+            x = 0;
+        }
+        setScrollX(x);
+    }
+
+    const handleRightArrow = () => {
+        let x = scrollX - Math.round(window.innerWidth / 2);
+        let listW = itens.results.length * 150;
+        if ((window.innerWidth - listW) > x) {
+            x = (window.innerWidth - listW) - 60;
+        }
+        setScrollX(x);
+    }
+
     return (
         <div className='movierow'>
             <h2>{title}</h2>
+            <div className='left' onClick={handleLeftArrow}>
+                <NavigateBeforeIcon style={{fontSize: 50}}/>
+            </div>
+            <div className='right' onClick={handleRightArrow}>
+                <NavigateNextIcon style={{fontSize: 50}}/>
+            </div>
+
             <div className='listarea'>
-                <div className='list'>
+                <div className='list' style={{marginLeft: scrollX, width: itens.results.length * 150}}>
                     {itens.results.length > 0 && itens.results.map((item, key) => (
                         <div key={key} className='item'>
                             <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
